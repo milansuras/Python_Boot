2d1349fdad775178c0e55d3ba76cc7366cacc6c7
MILAN_SURAS <milanmidhun2@gmail.com>
2024-05-22 20:11:37 +0530

    sum

---
+++ Loops/sum.py
@@ -0,0 +1,9 @@
+sum=0
+
+while True:
+    number=int(input("Enter a number to calculate -1 to quit := "))
+    if number==-1:
+        break
+    sum+=number
+
+print(f"Total sum of enetered number {sum}")
