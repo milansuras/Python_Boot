aca219a27baf6d77a5b8619b58dc7cd79fa3604a
MILAN_SURAS <milanmidhun2@gmail.com>
2024-06-27 19:49:00 +0530

    list operation example

---
+++ List/list01.py
@@ -0,0 +1,45 @@
+print("List Operation ")
+
+programming_Languages=["Python","Java","Golang","C++","C"]
+message=f"My first written programming language was {programming_Languages[-1].title()}"
+print(message)
+
+motorcycles=[]
+print(motorcycles)
+
+motorcycles.append("Splendor")
+print(motorcycles)
+
+motorcycles.insert(0,"Dominar")
+print(motorcycles)
+
+
+#removing an item by value
+
+values=[1,2,3,4,5,6,7]
+values.remove(6)
+print(values)
+
+# sorting a list
+
+numbers=[1,3,4,5,6,7,7,8,89,88]
+numbers.sort()
+print(numbers)
+
+char=["A","Z","J","Y"]
+char.sort()
+print(char)
+
+#looping through lists
+
+num=[1,2,3,4,5,6,7,7,89,9,0,0]
+
+for n in num:
+    print(n)
+
+milan={'name': 'milan', 'age': '25'}
+print(milan)
+
+
+
+
